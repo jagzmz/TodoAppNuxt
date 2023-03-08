5465ecaea0b9875ab37d2afb86aeba201a86d4ff
Mahesh Jaganiya <jagzmz@gmail.com>
2023-03-09 00:28:01 +0530

    test(App): fix test warnings

--- test/component/BaseModal.spec.js
+++ test/component/BaseModal.spec.js
@@ -1,16 +1,33 @@
 import { mount } from '@vue/test-utils'
 import { BaseModal } from '@/components/modals'
 
+const labelOk = 'Confirm'
+const labelCancel = 'Abort'
+
 describe('BaseModal', () => {
   it('emits "ok" event when ok button is clicked', async () => {
-    const wrapper = mount(BaseModal)
+    const wrapper = mount(BaseModal, {
+      propsData: {
+        modalProps: {
+          labelOk,
+          labelCancel,
+        },
+      },
+    })
     const okButton = wrapper.find('[data-testid="ok-button"]')
     await okButton.trigger('click')
     expect(wrapper.emitted('ok')).toBeTruthy()
   })
 
   it('emits "cancel" event when cancel button is clicked', async () => {
-    const wrapper = mount(BaseModal)
+    const wrapper = mount(BaseModal, {
+      propsData: {
+        modalProps: {
+          labelOk,
+          labelCancel,
+        },
+      },
+    })
     const cancelButton = wrapper.find('[data-testid="cancel-button"]')
     await cancelButton.trigger('click')
     expect(wrapper.emitted('cancel')).toBeTruthy()
@@ -26,14 +43,19 @@ describe('BaseModal', () => {
   })
 
   it('renders default button labels', () => {
-    const wrapper = mount(BaseModal)
+    const wrapper = mount(BaseModal, {
+      propsData: {
+        modalProps: {
+          labelOk,
+          labelCancel,
+        },
+      },
+    })
     expect(wrapper.find('[data-testid="ok-button"]').text()).toBe('Ok')
     expect(wrapper.find('[data-testid="cancel-button"]').text()).toBe('Cancel')
   })
 
   it('renders custom button labels', () => {
-    const labelOk = 'Confirm'
-    const labelCancel = 'Abort'
     const wrapper = mount(BaseModal, {
       propsData: {
         modalProps: {
